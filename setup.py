7a3a5078a95e579b9393c04155dba7a0916aa6b8
Damian Hites <nizebulous@gmail.com>
2016-11-13 22:02:59 -0500

    Fixing build issues.

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
     name='django_site_health',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         "Django>=1.8",
