897bef3169c9057bb30bad600ea30a2f1ea48262
Damian Hites <nizebulous@gmail.com>
2016-11-15 19:48:48 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django_site_health',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         "Django>=1.8",
